3754c25c87dbb6e381aa7938a4c6397229becce3
Ithyx <lamidey.m@gmail.com>
2021-05-23 12:28:08 -0400

    Bouton de connexion test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Yatga';
+	title = 'Yatga';
+
+	onSignIn(googleUser: any): void {
+		console.log('Google Auth Response', googleUser);
+	}
 }
