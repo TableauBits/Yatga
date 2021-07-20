434cb211f43e672ecd95cb93c5068be7df95b700
Ithyx <lamidey.m@gmail.com>
2021-07-19 22:48:08 -0400

    Ajout de la description pour le profil utilisateur
    Co-authored-by: Hugo VDBM <hugo.vdbm@icloud.com>

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -8,82 +8,86 @@ const DISPLAY_NAME_MIN_LENGTH = 1;
 const DISPLAY_NAME_MAX_LENGTH = 25;
 
 interface Status {
-  error: boolean;
-  hidden: boolean;
-  message: string;
+	error: boolean;
+	hidden: boolean;
+	message: string;
 }
 
 @Component({
-  selector: 'app-profile-page',
-  templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+	selector: 'app-profile-page',
+	templateUrl: './profile-page.component.html',
+	styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent {
 
-  public errorStatus: Status;
-  public profileForm: FormGroup;
+	public errorStatus: Status;
+	public profileForm: FormGroup;
 
-  constructor(public auth: AuthService, public fb: FormBuilder) {
-    this.profileForm = this.fb.group({
-      displayName: [this.isAlreadyAuth() ? this.auth.user.displayName : "", Validators.required],
-      photoURL: [this.isAlreadyAuth() ? this.auth.user.photoURL : "", Validators.required]
-    })
-    this.errorStatus = {
-      error: false,
-      hidden: true,
-      message: "",
-    }
-    this.auth.waitForAuth(() => {}, this.onConnect, this);
-  }
+	constructor(public auth: AuthService, public fb: FormBuilder) {
+		this.profileForm = this.fb.group({
+			displayName: [this.isAlreadyAuth() ? this.auth.user.displayName : "", Validators.required],
+			photoURL: [this.isAlreadyAuth() ? this.auth.user.photoURL : "", Validators.required],
+			description: [this.isAlreadyAuth() ? this.auth.user.description : ""],
+		})
+		this.errorStatus = {
+			error: false,
+			hidden: true,
+			message: "",
+		}
+		this.auth.waitForAuth(() => { }, this.onConnect, this);
+	}
 
-  private isAlreadyAuth(): boolean {
-    return this.auth.user !== EMPTY_USER;
-  }
+	private isAlreadyAuth(): boolean {
+		return this.auth.user !== EMPTY_USER;
+	}
 
-  private onConnect(): void {
-    this.profileForm.get("displayName")?.setValue(this.auth.user.displayName);
-    this.profileForm.get("photoURL")?.setValue(this.auth.user.photoURL);
-  }
+	private onConnect(): void {
+		this.profileForm.get("displayName")?.setValue(this.auth.user.displayName);
+		this.profileForm.get("photoURL")?.setValue(this.auth.user.photoURL);
+		this.profileForm.get("description")?.setValue(this.auth.user.description);
+	}
 
-  notifyFailure(message: string): void {
-    this.errorStatus.hidden = false;
-    this.errorStatus.error = true;
-    this.errorStatus.message = message;
-  }
+	notifyFailure(message: string): void {
+		this.errorStatus.hidden = false;
+		this.errorStatus.error = true;
+		this.errorStatus.message = message;
+	}
 
-  clearStatus(): void {
-    this.errorStatus.hidden = true;
-    this.errorStatus.error = false;
-    this.errorStatus.message = '';
-  }
+	clearStatus(): void {
+		this.errorStatus.hidden = true;
+		this.errorStatus.error = false;
+		this.errorStatus.message = '';
+	}
 
-  updateProfile(): void {
-    const displayName = this.profileForm.get('displayName');
-    const photoURL = this.profileForm.get('photoURL');
+	updateProfile(): void {
+		const displayName = this.profileForm.get('displayName');
+		const photoURL = this.profileForm.get('photoURL');
+		const description = this.profileForm.get('description');
 
-    const newUser: User = {...this.auth.user};
+		const newUser: User = { ...this.auth.user };
 
-    if (displayName) {
-      const value = displayName.value as string;
-      // TODO : server failure ?
-      if (value.length < DISPLAY_NAME_MIN_LENGTH || value.length > DISPLAY_NAME_MAX_LENGTH) {
-        this.notifyFailure(`Le nouveau nom d'utilisateur est invalide (Doit être entre ${DISPLAY_NAME_MIN_LENGTH} et ${DISPLAY_NAME_MAX_LENGTH} caractères)`)
-      } else {
-        newUser.displayName =  value;
-        this.clearStatus();
-      }
-    } 
-    if (photoURL) newUser.photoURL = photoURL.value;
+		if (displayName) {
+			const value = displayName.value as string;
+			// TODO : server failure ?
+			if (value.length < DISPLAY_NAME_MIN_LENGTH || value.length > DISPLAY_NAME_MAX_LENGTH) {
+				this.notifyFailure(`Le nouveau nom d'utilisateur est invalide (Doit être entre ${DISPLAY_NAME_MIN_LENGTH} et ${DISPLAY_NAME_MAX_LENGTH} caractères)`)
+			} else {
+				newUser.displayName = value;
+				this.clearStatus();
+			}
+		}
+		if (photoURL) newUser.photoURL = photoURL.value;
+		if (description) newUser.description = description.value;
 
-    if (this.errorStatus.hidden) {
-      const editProfileMessage = createMessage(EventTypes.USER_edit, {userData: newUser});
-      this.auth.ws.send(editProfileMessage);
-    }
-  }
+		if (this.errorStatus.hidden) {
+			const editProfileMessage = createMessage(EventTypes.USER_edit, { userData: newUser });
+			this.auth.ws.send(editProfileMessage);
+		}
+	}
 
-  getRoles(): Role[] {
-    const roles = returnUserRoles(this.auth.user.roles)
-    return roles ? roles : [];
-  }
+	getRoles(): Role[] {
+		const roles = returnUserRoles(this.auth.user.roles)
+		return roles ? roles : [];
+	}
 
 }
