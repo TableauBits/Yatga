5d000f2f21635839e26dde0483de0ec8505a9d23
Ithyx <lamidey.m@gmail.com>
2021-08-01 14:00:04 -0400

    Ajout de la liste des constitutions
    Co-authored-by: Hugo VDBM <hugo.vdbm@icloud.com>

--- src/app/components/current-constitution-page/current-constitution-page.component.ts
+++ src/app/components/current-constitution-page/current-constitution-page.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AuthService, ReqUserGet, ResUserUpdate } from 'src/app/services/auth.service';
+import { Constitution } from 'src/app/types/constitution';
+import { createMessage, EventTypes, Message } from 'src/app/types/message';
+import { EMPTY_USER, User } from 'src/app/types/user';
+
+type ReqCstGetFromUser = {}
+type ReqCstUnsubscribe = {
+	ids: string[]
+}
+
+type ResConstitutionUpdate = {
+	cstInfo: Constitution;
+};
+
+enum ConstitutionStatus {
+	JOINABLE = "JOINABLE",
+	JOINED = "JOINED",
+	FULL = "FULL",
+}
+
+interface DisplayData {
+	constitution: Constitution,
+	owner: User,
+}
 
 @Component({
-  selector: 'app-current-constitution-page',
-  templateUrl: './current-constitution-page.component.html',
-  styleUrls: ['./current-constitution-page.component.scss']
+	selector: 'app-current-constitution-page',
+	templateUrl: './current-constitution-page.component.html',
+	styleUrls: ['./current-constitution-page.component.scss']
 })
-export class CurrentConstitutionPageComponent implements OnInit {
+export class CurrentConstitutionPageComponent implements OnDestroy {
 
-  constructor() { }
+	public constitutions: Map<string, DisplayData>;
 
-  ngOnInit(): void {
-  }
+	constructor(private auth: AuthService) {
+		this.constitutions = new Map();
+		auth.waitForAuth(this.handleEvents, this.onConnect, this);
+	}
 
+	private handleEvents(event: MessageEvent<any>): void {
+		let message = JSON.parse(event.data.toString()) as Message<unknown>;
+		switch (message.event) {
+			case EventTypes.CST_update: {
+				const data = (message.data as ResConstitutionUpdate).cstInfo;
+
+				if (!this.constitutions.has(data.id) && data.users.length > 0) {
+					this.auth.ws.send(createMessage<ReqUserGet>(EventTypes.USER_get, { uids: [data.users[0]] }))
+				}
+				this.constitutions.set(data.id, { constitution: data, owner: EMPTY_USER })
+			} break;
+
+			case EventTypes.USER_update: {
+				const user = (message.data as ResUserUpdate).userInfo;
+				this.constitutions.forEach((data) => {
+					if (data.constitution.users[0] === user.uid) {
+						data.owner = user;
+					}
+				})
+			} break;
+
+			default: { } break;
+		};
+
+	}
+
+	private onConnect(): void {
+		this.auth.ws.send(createMessage<ReqCstGetFromUser>(EventTypes.CST_get_from_user, {}));
+	}
+
+	getConstitutions(): DisplayData[] {
+		return Array.from(this.constitutions.values());
+	}
+
+	getStatus(constitution: Constitution): ConstitutionStatus {
+		if (constitution.users.includes(this.auth.uid))
+			return ConstitutionStatus.JOINED;
+		if (constitution.users.length < constitution.maxUserCount)
+			return ConstitutionStatus.JOINABLE;
+		return ConstitutionStatus.FULL
+	}
+
+	ngOnDestroy() {
+		const allUIDs: Set<string> = new Set();
+		this.constitutions.forEach((data) => {
+			allUIDs.add(data.owner.uid);
+		})
+		console.log(allUIDs);
+		this.auth.ws.send(createMessage<ReqCstUnsubscribe>(EventTypes.CST_unsubscribe, { ids: Array.from(allUIDs.values()) }));
+	}
 }
--- src/app/types/constitution.ts
+++ src/app/types/constitution.ts
@@ -1,24 +1,26 @@
-export enum ConstitutionType  {
-  GRADE,
+export enum ConstitutionType {
+	GRADE,
 }
 
 // TODO : Better names ?
 export enum AnonymousLevel {
-  PUBLIC,
-  NO_USERNAME,
-  SOUND_ONLY
+	PUBLIC,
+	NO_USERNAME,
+	SOUND_ONLY
 }
 
 export interface Constitution {
-  id: string;
-  season: number;
-  part: number;
-  name: string;
-  isPublic: boolean;
-  anonymousLevel: AnonymousLevel;
-  type: ConstitutionType;
-  state: number;
+	id: string;
+	season: number;
+	part: number;
+	name: string;
+	isPublic: boolean;
+	anonymousLevel: AnonymousLevel;
+	type: ConstitutionType;
+	state: number;
+	playlistLink: string;
 
-  users: string[];                // user[0] as owner
-  numberOfSongsPerUser: number;
+	users: string[];                // user[0] as owner
+	maxUserCount: number;
+	numberOfSongsPerUser: number;
 }
