73e023046c00ca63585c21a5d45b2a1ff32ccd92
Ithyx <lamidey.m@gmail.com>
2021-08-21 13:06:50 -0400

    tout cassé
    Co-authored-by: Hugo VDBM <hugo.vdbm@icloud.com>

--- src/app/components/new-constitution/new-constitution.component.ts
+++ src/app/components/new-constitution/new-constitution.component.ts
@@ -5,30 +5,33 @@ import { isNumber } from 'lodash';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-new-constitution',
-  templateUrl: './new-constitution.component.html',
-  styleUrls: ['./new-constitution.component.scss']
+	selector: 'app-new-constitution',
+	templateUrl: './new-constitution.component.html',
+	styleUrls: ['./new-constitution.component.scss']
 })
-export class NewConstitutionComponent  {
+export class NewConstitutionComponent {
 
-  public newConstitutionForm: FormGroup;
+	public newConstitutionForm: FormGroup;
 
-  constructor(public auth: AuthService, public fb: FormBuilder) {
-    this.newConstitutionForm = this.fb.group({
-      season: [, Validators.required],
-      part: [, Validators.required],
-      name: ["", Validators.required],
-      isPublic: [true, Validators.required],
-      anonymousLevel: [AnonymousLevel.PUBLIC, Validators.required],
-      type: [ConstitutionType.GRADE, Validators.required],
-      playlistLink: ["", Validators.required],
-      maxUserCount: [4, Validators.required],
-      numberOfSongsPerUser: [1, Validators.required],
-    })
-  }
+	constructor(public auth: AuthService, public fb: FormBuilder) {
+		this.newConstitutionForm = this.fb.group({
+			season: [, Validators.required],
+			part: [, Validators.required],
+			name: ["", Validators.required],
+			isPublic: [true, Validators.required],
+			anonymousLevel: [AnonymousLevel.PUBLIC, Validators.required],
+			type: [ConstitutionType.GRADE, Validators.required],
+			playlistLink: ["", Validators.required],
+			maxUserCount: [4, Validators.required],
+			numberOfSongsPerUser: [1, Validators.required],
+		})
+	}
 
-  public getTypes(): string[] {
-    return Object.keys(ConstitutionType).filter((key) => isNaN(key as unknown as number)).splice(0, ConstitutionType.LENGTH);
-  }
+	public getTypes(): string[] {
+		return Object.keys(ConstitutionType).filter((key) => isNaN(key as unknown as number)).splice(0, ConstitutionType.LENGTH);
+	}
 
+	public getAnonymityLevels(): string[] {
+		return ["Toutes informations révélées", "Utilisateur caché", "Audio seulement"];
+	}
 }
