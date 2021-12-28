b0f186762d80acc1b4ba9badcdd1a17822b981f2
Ithyx <lamidey.m@gmail.com>
2021-12-27 19:31:47 -0500

    Fix de l'affichage des votes dans le popup La reprise est rude

--- src/app/components/constitution-page/votes/votes-grade/vote-navigator/vote-navigator.component.ts
+++ src/app/components/constitution-page/votes/votes-grade/vote-navigator/vote-navigator.component.ts
@@ -1,81 +1,99 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { createMessage, EventType, GradeReqEdit, GradeUserData, Song } from 'chelys';
+import { createMessage, EventType, extractMessageData, GradeReqEdit, GradeResUserDataUpdate, GradeUserData, Message, Song } from 'chelys';
 import { AuthService } from 'src/app/services/auth.service';
 import { getEmbedURL } from 'src/app/types/url';
+import { toMapNumber } from 'src/app/types/utils';
 
 interface VoteNavigatorInjectedData {
-  cstId: string,
-  currentSong: Song,
-  songs: Song[],
-  currentVote: number,
-  votes: GradeUserData,
+	cstId: string,
+	currentSong: Song,
+	songs: Song[],
+	currentVote: number,
+	votes: GradeUserData,
 }
 
 @Component({
-  selector: 'app-vote-navigator',
-  templateUrl: './vote-navigator.component.html',
-  styleUrls: ['./vote-navigator.component.scss']
+	selector: 'app-vote-navigator',
+	templateUrl: './vote-navigator.component.html',
+	styleUrls: ['./vote-navigator.component.scss']
 })
-export class VoteNavigatorComponent {
+export class VoteNavigatorComponent implements OnDestroy {
 
-  cstId: string;
+	cstId: string;
 
-  currentSong: Song;
-  currentSongSafeURL: SafeResourceUrl;
-  currentVote: number | undefined;
+	currentSong: Song;
+	currentSongSafeURL: SafeResourceUrl;
+	currentVote: number | undefined;
 
-  songs: Song[];
-  votes: GradeUserData;
+	songs: Song[];
+	votes: GradeUserData;
 
-  constructor(
-    private auth: AuthService,
-    private sanitizer: DomSanitizer,
-    private dialogRef: MatDialogRef<VoteNavigatorComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: VoteNavigatorInjectedData
-  ) {
-    this.cstId = data.cstId;
-    this.currentSong = data.currentSong;
-    this.currentVote = data.currentVote;
-    this.songs = data.songs;
-    this.votes = data.votes;
-    this.currentSongSafeURL = getEmbedURL(this.currentSong, this.sanitizer);
-  }
+	constructor(
+		private auth: AuthService,
+		private sanitizer: DomSanitizer,
+		private dialogRef: MatDialogRef<VoteNavigatorComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: VoteNavigatorInjectedData
+	) {
+		this.cstId = data.cstId;
+		this.currentSong = data.currentSong;
+		this.currentVote = data.currentVote;
+		this.songs = data.songs;
+		this.votes = data.votes;
+		this.currentSongSafeURL = getEmbedURL(this.currentSong, this.sanitizer);
 
-  vote(grade: number) {
-    const message = createMessage<GradeReqEdit>(EventType.CST_SONG_GRADE_edit, {cstId: this.cstId, voteData: {grade: grade, songId: this.currentSong.id}});
-    this.auth.ws.send(message);
-    this.currentVote = grade; // TODO : Necessary ?
-  }
+		this.auth.pushEventHandler(this.handleEvent, this);
+	}
 
-  array(n: number): any[] {
-    return Array(n);
-  }
+	ngOnDestroy(): void {
+		this.auth.popEventHandler();
+	}
 
-  isSelected(grade: number): boolean {
-    return grade + 1  === this.currentVote;
-  }
+	handleEvent(event: MessageEvent<any>): void {
+		let message = JSON.parse(event.data.toString()) as Message<unknown>;
 
-  previousSongExist(): boolean {
-    const index = this.songs.lastIndexOf(this.currentSong);
-    return index - 1 >= 0;
-  }
+		switch (message.event) {
+			case EventType.CST_SONG_GRADE_userdata_update: {
+				const data = extractMessageData<GradeResUserDataUpdate>(message).userData;
+				this.votes = { uid: data.uid, values: toMapNumber<number>(data.values) };
+			} break;
+		}
+	}
 
-  nextSongExist(): boolean {
-    const index = this.songs.lastIndexOf(this.currentSong);
-    return index + 1 < this.songs.length;
-  }
+	vote(grade: number) {
+		const message = createMessage<GradeReqEdit>(EventType.CST_SONG_GRADE_edit, { cstId: this.cstId, voteData: { grade: grade, songId: this.currentSong.id } });
+		this.auth.ws.send(message);
+		this.currentVote = grade; // TODO : Necessary ?
+	}
 
-  changeSong(shift: number): void {
-    const currentIndex = this.songs.lastIndexOf(this.currentSong);
+	array(n: number): any[] {
+		return Array(n);
+	}
 
-    this.currentSong = this.songs[currentIndex + shift];
-    this.currentVote = this.votes.values.get(this.currentSong.id);
-    this.currentSongSafeURL = getEmbedURL(this.currentSong, this.sanitizer);
-  }
+	isSelected(grade: number): boolean {
+		return grade + 1 === this.currentVote;
+	}
 
-  closeWindow(): void {
+	previousSongExist(): boolean {
+		const index = this.songs.lastIndexOf(this.currentSong);
+		return index - 1 >= 0;
+	}
+
+	nextSongExist(): boolean {
+		const index = this.songs.lastIndexOf(this.currentSong);
+		return index + 1 < this.songs.length;
+	}
+
+	changeSong(shift: number): void {
+		const currentIndex = this.songs.lastIndexOf(this.currentSong);
+
+		this.currentSong = this.songs[currentIndex + shift];
+		this.currentVote = this.votes.values.get(this.currentSong.id);
+		this.currentSongSafeURL = getEmbedURL(this.currentSong, this.sanitizer);
+	}
+
+	closeWindow(): void {
 		this.dialogRef.close();
 	}
 
--- src/app/components/constitution-page/votes/votes-grade/votes-grade.component.ts
+++ src/app/components/constitution-page/votes/votes-grade/votes-grade.component.ts
@@ -22,146 +22,146 @@ export class VotesGradeComponent implements OnDestroy {
 	@Input() users: Map<string, User> = new Map();
 	@Input() songs: Map<number, Song> = new Map();
 
-  safeUrls: Map<number, SafeResourceUrl> = new Map();
+	safeUrls: Map<number, SafeResourceUrl> = new Map();
 	currentIframeSongID: number;
 
-  votes: GradeUserData;
-  histogramGrades: number[];
-  summary: GradeSummary;
+	votes: GradeUserData;
+	histogramGrades: number[];
+	summary: GradeSummary;
 
-  cstID = "";
+	cstID = "";
 
-  cardsSortASC: boolean;
-  cardsViewEnabled: boolean;
-  showStats: boolean;
-  showAlreadyVoted: boolean;
+	cardsSortASC: boolean;
+	cardsViewEnabled: boolean;
+	showStats: boolean;
+	showAlreadyVoted: boolean;
 
-  constructor(
-    private auth: AuthService, 
-    private sanitizer: DomSanitizer,
-    private dialog: MatDialog,
-    private route: ActivatedRoute,
-  ) { 
-    this.currentIframeSongID = -1;
-    this.votes = { uid: this.auth.uid, values: new Map() };
-    this.summary = { voteCount: 0, userCount: new Map() };
-    this.histogramGrades = [];
-    this.cardsSortASC = (localStorage.getItem(CARDS_SORT_KEY) ?? true) === "false";
-    this.cardsViewEnabled = (localStorage.getItem(CARDS_VIEW_KEY) ?? true) === "true";
-    this.showStats = (localStorage.getItem(GRADE_SHOW_STATS_KEY) ?? true) === "true";
-    this.showAlreadyVoted = (localStorage.getItem(GRADE_ALREADY_VOTES_KEY) ?? true) === "true";
-    
-    this.auth.pushAuthFunction(this.onConnect, this);
+	constructor(
+		private auth: AuthService,
+		private sanitizer: DomSanitizer,
+		private dialog: MatDialog,
+		private route: ActivatedRoute,
+	) {
+		this.currentIframeSongID = -1;
+		this.votes = { uid: this.auth.uid, values: new Map() };
+		this.summary = { voteCount: 0, userCount: new Map() };
+		this.histogramGrades = [];
+		this.cardsSortASC = (localStorage.getItem(CARDS_SORT_KEY) ?? true) === "false";
+		this.cardsViewEnabled = (localStorage.getItem(CARDS_VIEW_KEY) ?? true) === "true";
+		this.showStats = (localStorage.getItem(GRADE_SHOW_STATS_KEY) ?? true) === "true";
+		this.showAlreadyVoted = (localStorage.getItem(GRADE_ALREADY_VOTES_KEY) ?? true) === "true";
+
+		this.auth.pushAuthFunction(this.onConnect, this);
 		this.auth.pushEventHandler(this.handleEvents, this);
-  }
+	}
 
 	ngOnDestroy(): void {
 		this.auth.popEventHandler();
 		this.auth.popAuthCallback();
 	}
 
-  private onConnect(): void {
-    this.route.params.subscribe((params) => {
+	private onConnect(): void {
+		this.route.params.subscribe((params) => {
 			this.cstID = params.cstID;
-      const messageGetUserVotes = createMessage<GradeReqGetUser>(EventType.CST_SONG_GRADE_get_user, { cstId: this.cstID});
-      this.auth.ws.send(messageGetUserVotes);
-  
-      const messageGetSummary = createMessage<GradeReqGetSummary>(EventType.CST_SONG_GRADE_get_summary, { cstId: this.cstID});
-      this.auth.ws.send(messageGetSummary);
-    });
-  }
+			const messageGetUserVotes = createMessage<GradeReqGetUser>(EventType.CST_SONG_GRADE_get_user, { cstId: this.cstID });
+			this.auth.ws.send(messageGetUserVotes);
 
-  private handleEvents(event: MessageEvent<any>): void {
-    let message = JSON.parse(event.data.toString()) as Message<unknown>;
-    
-    switch (message.event) {
-      case EventType.CST_SONG_GRADE_summary_update: {
-        const data = extractMessageData<GradeResSummaryUpdate>(message).summary;
-        this.summary = {voteCount: data.voteCount, userCount: toMap(data.userCount)};
-      }
-        break;
-      case EventType.CST_SONG_GRADE_userdata_update: {
-        const data = extractMessageData<GradeResUserDataUpdate>(message).userData;
-        this.votes = { uid: data.uid, values: toMapNumber<number>(data.values) };
-        this.histogramGrades = Array.from(this.votes.values.values());
-      }
-        break;
-      case EventType.CST_update: {
-        const cst = extractMessageData<CstResUpdate>(message).cstInfo;
+			const messageGetSummary = createMessage<GradeReqGetSummary>(EventType.CST_SONG_GRADE_get_summary, { cstId: this.cstID });
+			this.auth.ws.send(messageGetSummary);
+		});
+	}
+
+	private handleEvents(event: MessageEvent<any>): void {
+		let message = JSON.parse(event.data.toString()) as Message<unknown>;
+
+		switch (message.event) {
+			case EventType.CST_SONG_GRADE_summary_update: {
+				const data = extractMessageData<GradeResSummaryUpdate>(message).summary;
+				this.summary = { voteCount: data.voteCount, userCount: toMap(data.userCount) };
+			}
+				break;
+			case EventType.CST_SONG_GRADE_userdata_update: {
+				const data = extractMessageData<GradeResUserDataUpdate>(message).userData;
+				this.votes = { uid: data.uid, values: toMapNumber<number>(data.values) };
+				this.histogramGrades = Array.from(this.votes.values.values());
+			}
+				break;
+			case EventType.CST_update: {
+				const cst = extractMessageData<CstResUpdate>(message).cstInfo;
 				if (cst.id !== this.constitution.id) return;
 				this.constitution = cst;
-      }
-    }
-  }
+			}
+		}
+	}
 
-  updateGradeAlreadyVoted(): void {
+	updateGradeAlreadyVoted(): void {
 		this.showAlreadyVoted = !this.showAlreadyVoted;
 		localStorage.setItem(GRADE_ALREADY_VOTES_KEY, this.showAlreadyVoted.toString());
 	}
 
-  getSongsToVote(): Song[] {
-    const songsToVote = Array.from(this.songs.values()).filter(song => {
-      const isNotUserSong = song.user !== this.auth.uid;
-      const isAlreadyVoted = this.votes.values.has(song.id) && this.showAlreadyVoted
-      return isNotUserSong && !isAlreadyVoted;
-    });
-    if (this.cardsSortASC) return songsToVote.sort(compareSongASC)
+	getSongsToVote(): Song[] {
+		const songsToVote = Array.from(this.songs.values()).filter(song => {
+			const isNotUserSong = song.user !== this.auth.uid;
+			const isAlreadyVoted = this.votes.values.has(song.id) && this.showAlreadyVoted
+			return isNotUserSong && !isAlreadyVoted;
+		});
+		if (this.cardsSortASC) return songsToVote.sort(compareSongASC)
 		else return songsToVote.sort(compareSongDSC);
-  }
+	}
 
-  getGrades(): number[] {
-    return Array.from(this.votes.values.values());
-  }
+	getGrades(): number[] {
+		return Array.from(this.votes.values.values());
+	}
 
-  getVote(song: Song): number | undefined {
-    return this.votes.values.get(song.id);
-  }
+	getVote(song: Song): number | undefined {
+		return this.votes.values.get(song.id);
+	}
 
-  getMean(): string {
-    const values = Array.from(this.votes.values).map((value) => value[1]);
-    const meanValue = mean(values).toFixed(3)
-    return meanValue === 'NaN' ? '0.000' : meanValue;
-  }
+	getMean(): string {
+		const values = Array.from(this.votes.values).map((value) => value[1]);
+		const meanValue = mean(values).toFixed(3)
+		return meanValue === 'NaN' ? '0.000' : meanValue;
+	}
 
-  getVar(): string {
-    const values = Array.from(this.votes.values).map((value) => value[1]);
-    return variance(mean(values), values).toFixed(3);
-  }
+	getVar(): string {
+		const values = Array.from(this.votes.values).map((value) => value[1]);
+		return variance(mean(values), values).toFixed(3);
+	}
 
-  openVoteNavigator(song: Song): void {
-    const config = new MatDialogConfig();
+	openVoteNavigator(song: Song): void {
+		const config = new MatDialogConfig();
 
-    config.data = {
-      cstId: this.constitution.id,
+		config.data = {
+			cstId: this.constitution.id,
 			currentSong: song,
-      currentVote: this.getVote(song),
+			currentVote: this.getVote(song),
 			songs: this.getSongsToVote(),
-      votes: this.votes
+			votes: this.votes
 		}
 
-    this.dialog.open(VoteNavigatorComponent, config);
+		this.dialog.open(VoteNavigatorComponent, config);
 		this.currentIframeSongID = -1;
-  }
+	}
 
-  userVotesProgressBarValue(): number {
-    return this.votes.values.size / (this.constitution.numberOfSongsPerUser * (this.constitution.maxUserCount - 1)) * 100;
-  }
+	userVotesProgressBarValue(): number {
+		return this.votes.values.size / (this.constitution.numberOfSongsPerUser * (this.constitution.maxUserCount - 1)) * 100;
+	}
 
-  totalVotesProgressBarValue(): number {
-    return this.summary.voteCount / this.numberOfVotes() * 100;
-  }
+	totalVotesProgressBarValue(): number {
+		return this.summary.voteCount / this.numberOfVotes() * 100;
+	}
 
-  // TODO : Duplication de code //
+	// TODO : Duplication de code //
 
-  numberOfVotes(): number {
+	numberOfVotes(): number {
 		return this.constitution.maxUserCount * this.constitution.numberOfSongsPerUser * (this.constitution.maxUserCount - 1);
 	}
 
-  getUser(uid: string): User {
+	getUser(uid: string): User {
 		return this.users.get(uid) || EMPTY_USER;
 	}
 
-  getImageURL(song: Song): string {
+	getImageURL(song: Song): string {
 		switch (song.platform) {
 			case SongPlatform.YOUTUBE: {
 				const videoID = getIDFromURL(song);
@@ -174,11 +174,11 @@ export class VotesGradeComponent implements OnDestroy {
 		if (!this.safeUrls.has(song.id)) {
 			this.safeUrls.set(song.id, getEmbedURL(song, this.sanitizer));
 		}
-		
+
 		return this.safeUrls.get(song.id) || '';
 	}
 
-  updateCurrentIframeSong(song: Song): void {
+	updateCurrentIframeSong(song: Song): void {
 		this.currentIframeSongID = song.id;
 	}
 
