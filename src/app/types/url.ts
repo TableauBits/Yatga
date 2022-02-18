d69c4d31206fcbfd773a02f4c0ed472034c335fb
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-02-17 19:29:56 -0500

    Validation lors d'ajouts de chanson

--- src/app/types/url.ts
+++ src/app/types/url.ts
@@ -2,11 +2,13 @@ import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { Song, SongPlatform } from "chelys";
 import * as URLParse from "url-parse";
 
+export const DEFAULT_ID_FROM_URL = "i2-a5itIPy4";	// "X_dkdW3EG5Q" // "LmMfALLf1jo" // "dQw4w9WgXcQ";
+
 export function getIDFromURL(song: Song): string {
 	switch (song.platform) {
 		case SongPlatform.YOUTUBE: {
 			const parsedURL = new URLParse(song.url, true);
-			let videoID = "i2-a5itIPy4"; // "X_dkdW3EG5Q" // "LmMfALLf1jo" // "dQw4w9WgXcQ";
+			let videoID = DEFAULT_ID_FROM_URL;
 			if (parsedURL.hostname === "youtu.be") { videoID = parsedURL.pathname.split("/")[1] }
 			if (parsedURL.hostname === "www.youtube.com") { videoID = parsedURL.query["v"] ?? "" }
 			return videoID;
