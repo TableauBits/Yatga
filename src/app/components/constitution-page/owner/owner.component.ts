71f451c11d9ce4edc72980fb86ae9934346daee7
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-03-20 15:27:35 -0400

    init form

--- src/app/components/constitution-page/owner/owner.component.ts
+++ src/app/components/constitution-page/owner/owner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Constitution, ConstitutionType, EMPTY_CONSTITUTION, Song, User } from 'chelys';
 
@@ -7,7 +7,7 @@ import { Constitution, ConstitutionType, EMPTY_CONSTITUTION, Song, User } from '
 	templateUrl: './owner.component.html',
 	styleUrls: ['./owner.component.scss']
 })
-export class OwnerComponent {
+export class OwnerComponent implements OnChanges {
 
 	@Input() constitution: Constitution = EMPTY_CONSTITUTION;
 	@Input() users: Map<string, User> = new Map();
@@ -20,14 +20,17 @@ export class OwnerComponent {
 		return ConstitutionType;
 	}
 
+	ngOnChanges(): void {
+		this.constitutionParameterForm = this.fb.group({
+			name: [this.constitution.name, Validators.required], 
+			playlistLink: [this.constitution.playlistLink],
+		})
+	}
+
 	constructor(public fb: FormBuilder) {
 		this.constitutionParameterForm = this.fb.group({
 			name: [this.constitution.name, Validators.required], 
 			playlistLink: [this.constitution.playlistLink],
-
-			// required ?
-			isPublic: [this.constitution.isPublic, Validators.required],
-			anonymousLevel: [this.constitution.anonymousLevel, Validators.required],
 		})
 	}
 
