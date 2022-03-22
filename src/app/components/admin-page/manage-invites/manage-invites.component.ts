8cfb4590ce53a7d0d9362bab447b1011dc0b0e15
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-03-21 21:22:26 -0400

    invite

--- src/app/components/admin-page/manage-invites/manage-invites.component.ts
+++ src/app/components/admin-page/manage-invites/manage-invites.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Song, Invite } from 'chelys';
+import { Song, Invite, User, createMessage, EventType, Message, extractMessageData, InvResUpdate, InvReqNew, InvReqGetAll, UsrResUpdate, UsrReqGet, EMPTY_USER, InvReqDelete } from 'chelys';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -9,7 +9,9 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ManageInvitesComponent implements OnDestroy {
 
-  public invites: Invites[] = [];
+  // public invites: Map<string,Invite> = new Map;
+  public invites: Invite[] = [];
+  public users: Map<string, User> = new Map();
 
   constructor(private auth: AuthService) {
     this.auth.pushAuthFunction(this.onConnect, this);
@@ -21,8 +23,56 @@ export class ManageInvitesComponent implements OnDestroy {
 		this.auth.popAuthCallback();
   }
 
-  private handleEvents(event: MessageEvent<any>): void {}
+  private handleEvents(event: MessageEvent<any>): void {
+    let message = JSON.parse(event.data.toString()) as Message<unknown>;
+		switch (message.event) {
+      case EventType.INVITE_update: {
+        const {invite, status} = extractMessageData<InvResUpdate>(message);
+        
+        // this.invites.set(invite.id, invite);
+        if (status === "added") {
+          this.invites.push(invite);
+        } else if (status === "removed") {
+          const index = this.invites.findIndex((i) => i.id === invite.id);
+          this.invites.splice(index, 1);
+        }
 
-  private onConnect(): void {}
+        if (!this.users.has(invite.createdBy)) {
+          const getUserMessage = createMessage<UsrReqGet>(EventType.USER_get, {uids: [invite.createdBy]});
+          this.auth.ws.send(getUserMessage);
+        }
+        break;
+      }
+      case EventType.USER_update : {
+        const user = extractMessageData<UsrResUpdate>(message).userInfo;
+        this.users.set(user.uid, user);
+        break;
+      }
+    }
+  }
+
+  private onConnect(): void {
+    const getAllInvitesMessage = createMessage<InvReqGetAll>(EventType.INVITE_get_all, {})
+    this.auth.ws.send(getAllInvitesMessage);
+  }
+
+  newInvite(): void {
+    const newInviteMessage = createMessage<InvReqNew>(EventType.INVITE_new, {});
+    this.auth.ws.send(newInviteMessage);
+  }
+
+  deleteInvite(invite: Invite): void {
+    const deleteInviteMessage = createMessage<InvReqDelete>(EventType.INVITE_delete, {id: invite.id});
+    this.auth.ws.send(deleteInviteMessage);
+  }
+
+  formatDate(d: string): string {
+    const date = new Date(d);
+    return `(${d.substring(0, 10)} à ${date.getHours()}:${date.getMinutes()})`
+  }
+
+  getUser(uid: string): User {
+    return this.users.get(uid) || EMPTY_USER;
+  }
 
 }
