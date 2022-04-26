387b07138cc6725f227186aaf8b05aaebbf3340f
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-04-26 19:17:33 -0400

    end

--- src/app/components/constitution-page/export/export.component.ts
+++ src/app/components/constitution-page/export/export.component.ts
@@ -1,22 +1,37 @@
 // Source : https://stackblitz.com/edit/httpsstackoverflowcomquestions51806464how-to-create-and-downloa?file=src%2Fapp%2Fapp.component.ts
 import { Component, Input } from '@angular/core';
-import { Constitution, EMPTY_CONSTITUTION, Song, User } from 'chelys';
+import { Constitution, EMPTY_CONSTITUTION, Role, Song, User } from 'chelys';
+import { AuthService } from 'src/app/services/auth.service';
+import { compareSongASC } from 'src/app/types/song';
 
-enum ExportFormat {
-  NO_FORMAT,
-  CSV,
-  GOOGLE_SHEETS,
-  
+type ExportFormat = {
+  format: ExportValue;
+  name: string;
+  isDev: boolean;
 }
 
-const FORMATS = [
+enum ExportValue {
+  NO_FORMAT,
+  CSV,
+  CSV_DEV,
+  GOOGLE_SHEETS,
+}
+
+const FORMATS: ExportFormat[] = [
   {
-    format: ExportFormat.CSV,
-    name: 'CSV'
+    format: ExportValue.CSV,
+    name: 'CSV',
+    isDev: false
   },
   {
-    format: ExportFormat.GOOGLE_SHEETS,
-    name: 'Google Sheets'
+    format: ExportValue.CSV_DEV,
+    name: 'CSV (dev)',
+    isDev: true
+  },
+  {
+    format: ExportValue.GOOGLE_SHEETS,
+    name: 'Google Sheets',
+    isDev: false
   }
 ]
 
@@ -31,8 +46,8 @@ export class ExportComponent {
   @Input() users: Map<string, User> = new Map();
 	@Input() songs: Map<number, Song> = new Map();
 
-  availableFormat = FORMATS;
-  selectedFormat: ExportFormat;
+  availableFormat: ExportFormat[];
+  selectedFormat: ExportValue;
 
   private setting = {
     element: {
@@ -40,8 +55,13 @@ export class ExportComponent {
     }
   }
 
-  constructor() {
-    this.selectedFormat = ExportFormat.NO_FORMAT;
+  constructor(private auth: AuthService) {
+    this.selectedFormat = ExportValue.NO_FORMAT;
+
+    if (!this.auth.user.roles.includes(Role.DEV)) {
+      this.availableFormat = FORMATS.filter((f) => !f.isDev);
+    } else this.availableFormat = FORMATS;
+
   }
 
   getUsername(uid: string): string {
@@ -52,30 +72,39 @@ export class ExportComponent {
     return field.includes(',') ? `"${field}"` : field;
   }
 
-  toGoogleSheets(): string {
-    return Array.from(this.songs.values()).map((song) => `=HYPERLINK("${song.url}"; "${song.title}")`).join("\n")
+  toGoogleSheets(songs: Song[]): string {
+    return songs.map((song) => `=HYPERLINK("${song.url}"; "${song.title}")`).join("\n")
   }
 
-  toCSV(): string {
-    const header = "Title,Author,URL,User\n";
-
-    return header + Array.from(this.songs.values()).map((song) => {
-      return `${this.correctCSVField(song.title)},${this.correctCSVField(song.author)},${this.correctCSVField(song.url)},${this.correctCSVField(this.getUsername(song.user))}`;
-    }).join("\n")
+  toCSV(songs: Song[], devFormat: boolean): string {
+    if (devFormat) {
+      return Object.keys(songs[0]).sort().join(",") + "\n" + songs.map((song) => {
+        return Object.entries(song)
+          .sort()
+          .map((v) => this.correctCSVField(String(v[1])))
+          .join(",")
+      }).join("\n");
+    } else {
+      return "author,title,url,user\n" + songs.map((song) => {
+        return `${this.correctCSVField(song.author)},${this.correctCSVField(song.title)},${this.correctCSVField(song.url)},${this.correctCSVField(this.getUsername(song.user))}`;
+      }).join("\n")
+    }
   }
 
   download() {
+    const songs = Array.from(this.songs.values()).sort(compareSongASC);
     switch (Number(this.selectedFormat)) {
-      case ExportFormat.CSV: 
+      case ExportValue.CSV:
+      case ExportValue.CSV_DEV:
         this.dyanmicDownloadByHtmlTag({
           fileName: this.constitution.name + ".csv",
-          text: this.toCSV()
+          text: this.toCSV(songs, Number(this.selectedFormat) === ExportValue.CSV_DEV)
         })
         break;
-      case ExportFormat.GOOGLE_SHEETS:
+      case ExportValue.GOOGLE_SHEETS:
         this.dyanmicDownloadByHtmlTag({
           fileName: this.constitution.name + ".txt",
-          text: this.toGoogleSheets()
+          text: this.toGoogleSheets(songs)
         })
         break;
     }
