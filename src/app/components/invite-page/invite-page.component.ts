c234f6099c283bc8fe68a9a67ef40b193cda83c2
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-08-16 20:15:00 -0400

    start invite page

--- src/app/components/invite-page/invite-page.component.ts
+++ src/app/components/invite-page/invite-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-invite-page',
   templateUrl: './invite-page.component.html',
   styleUrls: ['./invite-page.component.scss']
 })
-export class InvitePageComponent implements OnInit {
+export class InvitePageComponent {
 
-  constructor() { }
+  public inviteForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(public fb: FormBuilder) {
+    this.inviteForm = this.fb.group({
+      inviteID: [, Validators.required]
+    });
   }
 
 }
