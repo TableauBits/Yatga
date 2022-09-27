5fdd75df26bae950c042f0db0a33befab8f0b122
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-09-27 16:30:26 -0400

    Get Releases

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+export interface Release {
+	body: string;
+	html_url: string;
+	name: string;
+	published_at: string;
+	tag_name: string;
+}
 
 @Component({
 	selector: 'app-home-page',
 	templateUrl: './home-page.component.html',
 	styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
-	constructor() { }
+	releases: Release[];
 
-	ngOnInit(): void {
+	constructor(private http: HttpClient) {
+		this.releases = [];
+		this.get() 
 	}
 
+	get(): void {
+		this.http.get('https://api.github.com/repos/TableauBits/Yatga/releases').subscribe((data: any) => {
+			this.releases = data;
+			console.log(this.releases)
+		});
+	}
 }
