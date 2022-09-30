a3d871b3e85eb28b3f3c14bebe447b71a95a8a6f
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-09-29 20:07:18 -0400

    release body

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,7 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { isNil } from 'lodash';
 
-export interface Release {
+interface Changes {
+	author: string;
+	title: string;
+	url: string;
+}
+
+interface BodyRelease {
+	body: string;
+	changes: Changes[];
+	full: string;
+}
+
+interface MatbayRelease {
+	body: BodyRelease;
+	date: string;
+	name: string;
+	url: string;
+	tag: string;
+}
+
+interface GithubRelease {
 	body: string;
 	html_url: string;
 	name: string;
@@ -15,18 +36,49 @@ export interface Release {
 	styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
-	releases: Release[];
+	releases: MatbayRelease[];
 
 	constructor(private http: HttpClient) {
 		this.releases = [];
-		this.get() 
+		this.get();
 	}
 
 	get(): void {
 		this.http.get('https://api.github.com/repos/TableauBits/Yatga/releases').subscribe((data: any) => {
-			this.releases = data;
-			console.log(this.releases)
+			this.releases = (data as GithubRelease[]).map(d => {
+				return {
+					body: this.cleanBody(d.body),
+					date: d.published_at.slice(0, 10),
+					name: d.name,
+					url: d.html_url,
+					tag: d.tag_name,
+				}
+			})
 		});
+	}
+
+	cleanBody(body: string): BodyRelease {
+		let full = "";
+		let changes: Changes[] = []
+		for(let sentence of body.split('\n')) {
+			if (sentence.includes("**Full Changelog**:")) {
+				const index = sentence.indexOf('https://github.com/TableauBits/Yatga');
+				full = sentence.substring(index);
+			} else if (sentence.startsWith("* ")) {
+				const reg = new RegExp(/\* (.*) by @(.*) in (.*)/gm);
+				const result = reg.exec(sentence);
+				if (isNil(result)) continue;
+				changes.push({
+					author: result[2],
+					title: result[1],
+					url: result[3],
+				})
+			}
+		}
+		return {
+			body,
+			changes,
+			full,
+		}
 	}
 }
