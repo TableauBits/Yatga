76c3580da9cf438d35cee60a2c3e960b604f35c7
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2022-09-30 11:38:04 -0400

    fix OR operator

--- src/app/components/constitution-page/manage-songs/manage-songs.component.ts
+++ src/app/components/constitution-page/manage-songs/manage-songs.component.ts
@@ -61,7 +61,8 @@ export class ManageSongsComponent {
 	private songUpdate(response: CstSongResUpdate) {
 		const songInfo = response.songInfo;
 		switch (response.status) {
-			case "added" || "modified":
+			case "added":
+			case "modified":
 				this.songs.set(songInfo.id, songInfo);
 				break;
 			case "removed":
