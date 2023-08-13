ac8757411167849f7aedd765363a46a1ab007716
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2023-08-13 13:26:40 -0400

    add navigator display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { ManageRolesComponent } from './components/admin-page/manage-roles/manag
 import { InvitePageComponent } from './components/invite-page/invite-page.component';
 import { OptionnalSongInfosButtonComponent } from './components/constitution-page/optionnal-song-infos-button/optionnal-song-infos-button.component';
 import { ResultsConstitutionComponent } from './components/constitution-page/results/results-constitution/results-constitution.component';
+import { NavigatorSongDisplayComponent } from './components/constitution-page/navigator-song-display/navigator-song-display.component';
 
 // Charts Component
 import { HistogramComponent } from './components/template/histogram/histogram.component';
@@ -140,6 +141,7 @@ import { AuthService } from './services/auth.service';
 		InvitePageComponent,
 		OptionnalSongInfosButtonComponent,
   ResultsConstitutionComponent,
+  NavigatorSongDisplayComponent,
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/components/constitution-page/navigator-song-display/navigator-song-display.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
+import { EMPTY_SONG, Song } from 'chelys';
+import { GetUrlService } from 'src/app/services/get-url.service';
+
+@Component({
+  selector: 'app-navigator-song-display',
+  templateUrl: './navigator-song-display.component.html',
+  styleUrls: ['./navigator-song-display.component.scss']
+})
+export class NavigatorSongDisplayComponent implements OnChanges {
+
+  @Input() song: Song = EMPTY_SONG;
+  songSafeURL: SafeResourceUrl = {};
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.songSafeURL = this.urlGetter.getEmbedURL(changes['song'].currentValue);
+  }
+
+  constructor(public urlGetter: GetUrlService) { }
+
+  getSubTitle(): string {
+    return this.song.album ? `${this.song.author} - ${this.song.album}` : this.song.author
+  }
+
+}
