c8ad8024399c1c9b57b0634216a907363bd0af10
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2023-09-07 18:21:17 -0400

    add "Autre" language

--- src/app/types/song-utils.ts
+++ src/app/types/song-utils.ts
@@ -4,7 +4,7 @@ import { capitalizeFirstLetter } from './utils';
 
 export const GRADE_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-export const ALL_LANGUAGES_FR = getAll639_1().map((code) => capitalizeFirstLetter(getName(code, "fr") as string)).sort();
+export const ALL_LANGUAGES_FR = getAll639_1().map((code) => capitalizeFirstLetter(getName(code, "fr") as string));
 export const LANGUAGES_FR_TO_CODE = new Map<string, string>();
 export const LANGUAGES_CODE_TO_FR = new Map<string, string>();
 export const INSTRUMENTAL_CODE = "ins";
@@ -16,7 +16,13 @@ getAll639_1().forEach((code) => {
 });
 
 // Special case
+/// Instrumental
 LANGUAGES_CODE_TO_FR.set(INSTRUMENTAL_CODE, "Instrumental");
+
+/// Autre
+ALL_LANGUAGES_FR.push("Autre");
+LANGUAGES_CODE_TO_FR.set("oth", "Autre");
+LANGUAGES_FR_TO_CODE.set("Autre", "oth");
 
 export const ALL_GENRES = MUSIC_GENRES.sort().filter((elem, index, self)=> {
   return index === self.indexOf(elem);
