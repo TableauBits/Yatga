af468e37179b36ee17eb697e6171c808fb3fe09b
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2023-10-03 20:20:18 -0400

    v1

--- src/app/components/constitution-page/results/results-grade/grade-grades/grade-grades.component.ts
+++ src/app/components/constitution-page/results/results-grade/grade-grades/grade-grades.component.ts
@@ -9,6 +9,18 @@ import { SongGrade, UserGradeResults } from 'src/app/types/results';
 import { GRADE_VALUES, LANGUAGES_CODE_TO_FR } from 'src/app/types/song-utils';
 import { keepUniqueValues } from 'src/app/types/utils';
 
+type UserGrade = {
+  uid: string;
+  songId: number;
+  grade: number;
+  normalizeScore: number;
+}
+
+type MinMaxGrades = {
+  min: UserGrade;
+  max: UserGrade;
+}
+
 @Component({
   selector: 'app-grade-grades',
   templateUrl: './grade-grades.component.html',
@@ -25,29 +37,40 @@ export class GradeGradesComponent implements OnChanges {
 
   histogramValues: number[] = [];
   selectedUser: string;
-  selecedUserMean: number = 0;
+  selectedUserMean: number = 0;
   selectedUserVar: number = 0;
 
   languagesScatterConfig: ScatterConfig = EMPTY_SCATTER_CONFIG;
 
+  minMaxGrades: MinMaxGrades;
+
   ngOnChanges(changes: SimpleChanges): void {
     this.userResults = changes['userResults'].currentValue;
-    this.histogramValues = this.getUserHistogramValues();   // Init values
+    this.generateHistogramValues();   // Init values
     this.generateScatterInfos();
+    this.generateMinMaxGrades();
   }
 
   constructor(private auth: AuthService, public urlGetter: GetUrlService) {
     this.selectedUser = auth.uid;
     this.selectedSong = '-1';
+    this.minMaxGrades = {
+      max: {uid: "", songId: -1, normalizeScore: 0, grade: -1},
+      min: {uid: "", songId: -1, normalizeScore: 0, grade: -1}
+    };
   }
 
   getSongList(): Song[] {
     return Array.from(this.songs.values());
   }
 
+  getSong(id: number): Song {
+    return this.songs.get(id) ?? EMPTY_SONG;
+  }
+
   getSelectedSong(): Song {
     const id = toNumber(this.selectedSong);
-    return this.songs.get(id) || EMPTY_SONG;
+    return this.songs.get(id) ?? EMPTY_SONG;
   }
 
   getImageURL(): string {
@@ -56,19 +79,19 @@ export class GradeGradesComponent implements OnChanges {
 	}
 
   returnVote(uid: string): number | string {
-    return this.userResults.get(uid)?.data.values.get(toNumber(this.selectedSong)) || '/';
+    return this.userResults.get(uid)?.data.values.get(toNumber(this.selectedSong)) ?? '/';
   }
 
   returnScore(uid: string): number | string {
-    return this.userResults.get(uid)?.normalizeScores.get(toNumber(this.selectedSong))?.toFixed(4) || '/';
+    return this.userResults.get(uid)?.normalizeScores.get(toNumber(this.selectedSong))?.toFixed(4) ?? '/';
   }
 
   getUser(uid: string): User {
-    return this.users.get(uid) || EMPTY_USER;
+    return this.users.get(uid) ?? EMPTY_USER;
   }
 
   getSelectedUser(): User {
-    return this.users.get(this.selectedUser) || EMPTY_USER;
+    return this.users.get(this.selectedUser) ?? EMPTY_USER;
   }
 
   getSelectedUserSong(): SongGrade[] {
@@ -77,7 +100,7 @@ export class GradeGradesComponent implements OnChanges {
     let songs: SongGrade[] = [];
     userResult.data.values.forEach((value, key) => {
       songs.push({
-        song: this.songs.get(key) || EMPTY_SONG,
+        song: this.songs.get(key) ?? EMPTY_SONG,
         grade: value
       });
     });
@@ -89,17 +112,16 @@ export class GradeGradesComponent implements OnChanges {
     return Array.from(this.users.values());
   }
 
-  getUserHistogramValues(): number[] {
+  generateHistogramValues(): void {
     const userResult = this.userResults.get(this.selectedUser);
-    if (isNil(userResult)) return [];
-    const values = Array.from(userResult.data.values.values());
-    this.selecedUserMean = mean(values);
-    this.selectedUserVar = variance(this.selecedUserMean, values);
-    return values;
+    if (isNil(userResult)) return;
+    this.histogramValues = Array.from(userResult.data.values.values());
+    this.selectedUserMean = mean(this.histogramValues);
+    this.selectedUserVar = variance(this.selectedUserMean, this.histogramValues);
   }
 
   newSelection(): void  {
-    this.histogramValues = this.getUserHistogramValues();
+    this.generateHistogramValues();
     this.generateScatterInfos();
   }
 
@@ -121,13 +143,39 @@ export class GradeGradesComponent implements OnChanges {
       data: flatten(languages.map((language, index) => {
         let scatterPoints: ScatterData[] = [];
         range(1, 11).forEach(grade => {
-          const count = songs.filter(s => s.languages?.includes(language || "") && votes?.get(s.id) === grade).length;
+          const count = songs.filter(s => s.languages?.includes(language ?? "") && votes?.get(s.id) === grade).length;
           if (count === 0) return;
           scatterPoints.push([index, grade-1, count]);  // grade-1 because index should start at 0
         });
         return scatterPoints;
       })),
-      names: languages.map(language => LANGUAGES_CODE_TO_FR.get(language || "") || "")
+      names: languages.map(language => LANGUAGES_CODE_TO_FR.get(language ?? "") ?? "")
     };
+  }
+
+  generateMinMaxGrades(): void {
+    let maxGrade: UserGrade =  {uid: "", songId: -1, normalizeScore: 0, grade: -1};
+    let minGrade: UserGrade = {uid: "", songId: -1, normalizeScore: Infinity, grade: -1};
+    this.users.forEach(user => {
+      const result = this.userResults.get(user.uid);
+      if (isNil(result)) return;
+      this.songs.forEach(song => {
+        const score = result.normalizeScores.get(song.id);
+        if (isNil(score)) return;
+        if (maxGrade.normalizeScore < score) {
+          maxGrade = {uid: user.uid, songId: song.id, normalizeScore: score, grade: result.data.values.get(song.id) ?? -1};
+        } else if (minGrade.normalizeScore > score) {
+          minGrade = {uid: user.uid, songId: song.id, normalizeScore: score, grade: result.data.values.get(song.id) ?? -1};
+        }
+      });
+    });
+    this.minMaxGrades = {
+      min: minGrade,
+      max: maxGrade
+    };
+  }
+
+  propertyExists(property: keyof Song): boolean {
+    return Array.from(this.songs.values()).findIndex((s) => { return !isNil(s[property]); }) !== -1;
   }
 }
