32c7744547164282eb9cb37651d8489b8a173f45
Ithyx <lamidey.m@gmail.com>
2024-01-10 16:06:48 -0500

    Mise à jour de la méthode de connexion à kalimba suite aux changements sur Fly.io

---
+++ src/app/types/environment.ts
@@ -0,0 +1,21 @@
+type Environment = {
+    production: boolean;
+    firebase: {
+        apiKey: string;
+        authDomain: string;
+        databaseURL: string,
+        projectId: string,
+        storageBucket: string,
+        messagingSenderId: string,
+        appId: string,
+        measurementId: string
+    }
+    protocolHTTP: string;
+    protocolWebSocket: string;
+    portWebSocket?: string;
+    serverAPI: string;
+}
+
+export {
+    Environment
+}
