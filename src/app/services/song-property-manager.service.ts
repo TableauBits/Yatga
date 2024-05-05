68d4dbe14065ac7491e1a86efbf8e982cd9cd9a3
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2024-05-05 19:33:49 -0400

    Added getFeaturingList method

--- src/app/services/song-property-manager.service.ts
+++ src/app/services/song-property-manager.service.ts
@@ -10,6 +10,14 @@ export class SongPropertyManagerService {
 		window.open(song.url, "_blank");
 	}
 
+  getFeaturingList(song: Song): string {
+    if (!song.featuring || song.featuring.length === 0) return "";
+    if (song.featuring.length === 1) return song.featuring[0];
+
+    const last = song.featuring.pop();
+    return `${song.featuring.join(", ")} & ${last}`;
+  }
+
   getTitle(song: Song): string {
     let title = song.title;
     if (song.altTitles) title += ` (${song.altTitles.join(" / ")})`;
@@ -18,6 +26,7 @@ export class SongPropertyManagerService {
 
   getSubTitle(song: Song): string {
     let subtitle = song.author;
+    if (song.featuring) subtitle += ` feat. ${this.getFeaturingList(song)}`;
     if (song.album) subtitle += ` • ${song.album}`;
     if (song.releaseYear) subtitle += ` (${song.releaseYear})`;
     return subtitle;
