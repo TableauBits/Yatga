0c82cb17e74cafb544c8db242e245fc2916a428c
Aezurly <alice.vergeau@gmail.com>
2025-03-27 23:37:29 -0400

    navbar fix

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,15 +3,28 @@ import { MatDialog } from '@angular/material/dialog';
 import { Role } from 'chelys';
 import { AuthService } from 'src/app/services/auth.service';
 import { JoinConstitutionComponent } from '../join-constitution/join-constitution.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.scss']
+	styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+	isSmallScreen = false;
+	constructor(
+		public auth: AuthService,
+		private dialog: MatDialog,
+		private breakpointObserver: BreakpointObserver
+	) {}
 
-	constructor(public auth: AuthService, private dialog: MatDialog) {}
+	ngOnInit(): void {
+		this.breakpointObserver
+			.observe([Breakpoints.Handset])
+			.subscribe((result) => {
+				this.isSmallScreen = result.matches;
+			});
+	}
 
 	isAuthorized(): boolean {
 		return this.auth.user.roles.includes(Role.ADMIN);
@@ -22,6 +35,6 @@ export class NavbarComponent {
 	}
 
 	navigateTo(url: string) {
-		window.open(url, "_blank");
+		window.open(url, '_blank');
 	}
 }
