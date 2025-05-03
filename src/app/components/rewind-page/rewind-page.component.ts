7e4ddb3a7cfb874558cf22e8c01fa936ca178dd2
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2025-05-02 23:39:48 -0400

    v1

--- src/app/components/rewind-page/rewind-page.component.ts
+++ src/app/components/rewind-page/rewind-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { createMessage, EventType, extractMessageData, Message, RewindPerYear, RwdReqGet, RwdResUpdate } from 'chelys';
+import { createMessage, EMPTY_USER, EventType, extractMessageData, Message, RewindPerYear, RwdReqGet, RwdResUpdate, User, UsrReqGet, UsrResUpdate } from 'chelys';
+import { isNil } from 'lodash';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,28 +9,57 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./rewind-page.component.scss']
 })
 export class RewindPageComponent {
-
   public rewinds: Map<number, RewindPerYear> = new Map();
+
+  public selectedYear?: number;
+  public selectedRewind?: RewindPerYear;
+
+  private users: Map<string, User> = new Map();
 
   constructor(public auth: AuthService) {
     this.auth.pushAuthFunction(this.onConnect, this);
-		this.auth.pushEventHandler(this.handleEvents, this);
+    this.auth.pushEventHandler(this.handleEvents, this);
   }
 
   private handleEvents(event: MessageEvent<any>): void {
     let message = JSON.parse(event.data.toString()) as Message<unknown>;
-    if (message.event !== EventType.REWIND_update) return;
 
-    const data = extractMessageData<RwdResUpdate>(message);
+    switch (message.event) {
+      case EventType.REWIND_update: {
+        const data = extractMessageData<RwdResUpdate>(message);
 
-    this.rewinds.set(data.year, data.rewind);
+        this.rewinds.set(data.year, data.rewind);
+        if (this.selectedYear === undefined || this.selectedYear < data.year) {
+          this.selectedYear = data.year;
+          this.selectedRewind = data.rewind;
+        }
+      } break;
+
+      case EventType.USER_update: {
+        const data = extractMessageData<UsrResUpdate>(message).userInfo;
+        this.users.set(data.uid, data);
+      } break;
+    }
   }
 
   private onConnect(): void {
-    this.auth.ws.send(createMessage<RwdReqGet>(EventType.REWIND_get, {uid: this.auth.uid}));
+    this.auth.ws.send(
+      createMessage(EventType.USER_get_all, {})
+    );
+
+    this.auth.ws.send(
+      createMessage<RwdReqGet>(EventType.REWIND_get, { uid: this.auth.uid }));
   }
 
-  listRewinds(): number[] {
-    return Array.from(this.rewinds.keys());
+  getUser(uid: string): User {
+    const user = this.users.get(uid);
+    if (isNil(user)) return EMPTY_USER;
+    return user;
+  }
+
+  selectYear(): void {
+    const year = this.selectedYear;
+    if (isNil(year)) return;
+    this.selectedRewind = this.rewinds.get(year);
   }
 }
