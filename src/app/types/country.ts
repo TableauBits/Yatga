cf7a54a2ca64482a91c9eeeb4dc8fa6f996cd4e4
hvdbm <hugo.vdbm@icloud.com>
2025-06-30 17:56:38 -0400

    Ajout traduction française pour le nom des pays (#183)

--- src/app/types/country.ts
+++ src/app/types/country.ts
@@ -1,9 +1,9 @@
-import { countries } from 'countries-list';
+import * as COUNTRY_FR from '../../assets/countries.json';
 
 export const COUNTRY_NAMES_TO_CODE: Map<string, string> = new Map<string, string>();
 export const COUNTRY_CODES_TO_NAME: Map<string, string> = new Map<string, string>();
 
-for (const [key, value] of Object.entries(countries)) {
-  COUNTRY_CODES_TO_NAME.set(key, value.name);
-  COUNTRY_NAMES_TO_CODE.set(value.name, key);
+for (const [key, value] of Object.entries(COUNTRY_FR)) {
+  COUNTRY_CODES_TO_NAME.set(key, value);
+  COUNTRY_NAMES_TO_CODE.set(value, key);
 }
