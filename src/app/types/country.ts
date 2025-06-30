ce5620c3145ec5b12fbf5ee6618e746384b36f82
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2025-06-30 17:54:08 -0400

    Fix lint

--- src/app/types/country.ts
+++ src/app/types/country.ts
@@ -1,4 +1,4 @@
-import * as COUNTRY_FR from '../../assets/countries.json'
+import * as COUNTRY_FR from '../../assets/countries.json';
 
 export const COUNTRY_NAMES_TO_CODE: Map<string, string> = new Map<string, string>();
 export const COUNTRY_CODES_TO_NAME: Map<string, string> = new Map<string, string>();
