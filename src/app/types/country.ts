6c754a1945009d0cb0e182175f2b304fe80ff1a1
hvdbm <hugo.vdbm@icloud.com>
2025-09-30 20:55:28 -0400

    Correction autocompletion pour le nom de pays lors de l'ajout d'une musique  (#184)

--- src/app/types/country.ts
+++ src/app/types/country.ts
@@ -4,6 +4,7 @@ export const COUNTRY_NAMES_TO_CODE: Map<string, string> = new Map<string, string
 export const COUNTRY_CODES_TO_NAME: Map<string, string> = new Map<string, string>();
 
 for (const [key, value] of Object.entries(COUNTRY_FR)) {
+  if (key === "default") continue;
   COUNTRY_CODES_TO_NAME.set(key, value);
   COUNTRY_NAMES_TO_CODE.set(value, key);
 }
